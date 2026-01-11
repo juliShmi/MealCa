2d787d8c08f0815bd7f2a01ee14d1a68fb1a24bb
juliShmi <juliashmitko@gmail.com>
2026-01-11 19:06:59 +0200

    fix cooking time -- add hours and minutes

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,10 +1,26 @@
 function RecipeCard({ title, ingredients, time }) {
 
+  const formatTime = (minutes) => {
+    const hours = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+
+    if (hours === 0) {
+      return `${mins} min`;
+    }
+
+    if (mins === 0) {
+      return `${hours} h`;
+    }
+
+    return `${hours} h ${mins} min`;
+  };
+
+
   return (
     <div className="recipe-card">
       <h2>{title}</h2>
       <p><strong>Ingredients:</strong> {ingredients.join(', ')}</p>
-      <p><strong>Cooking time:</strong> {time} min</p>
+      <p><strong>Time:</strong> {formatTime(time)}</p>
     </div>
   );
 }
